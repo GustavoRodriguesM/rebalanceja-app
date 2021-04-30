b040467d41a491a8610e47f0fd80cceb9caf7266
Gustavo Rodrigues <gustavorodriguesm@hotmail.com>
2021-04-30 12:02:20 -0300

    Tentando melhorar a performance

--- src/screens/subscreens/stock-screen/AquisitionConfigSubscreen.js
+++ src/screens/subscreens/stock-screen/AquisitionConfigSubscreen.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { ScrollView, View } from 'react-native'
+import { ScrollView, View, FlatList, SectionList } from 'react-native'
 import { Paragraph, useTheme, Appbar, Portal, FAB, Provider } from 'react-native-paper'
 import { WalletService } from '../../../services/WalletService'
 import { AquisitionService } from '../../../services/AquisitionService'
 import CardStockComponent from '../../../components/stocks-components/CardStockComponent'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 
 export default props => {
@@ -15,7 +16,7 @@ export default props => {
     const onStateChange = ({ open }) => setState({ open });
     const { open } = state;
 
-    const  fetchMyAPI = async () => {
+    const fetchMyAPI = async () => {
         let walletLocal = await new WalletService().getActiveWallet();
         let walletStocks = await new WalletService().getAquisitionsByWallet(walletLocal.idWallet);
         setDescription(walletLocal.description)
@@ -23,16 +24,16 @@ export default props => {
     }
 
     useEffect(() => {
-        
-            fetchMyAPI();
-    },[])
+
+        fetchMyAPI();
+    }, [])
 
 
     useEffect(() => {
         props.navigation.addListener('focus', async () => {
             fetchMyAPI();
         });
-    },[])
+    }, [])
 
     const onToggleSwitch = async (obj, index) => {
         let aquisitionsLocal = [...aquisitions]
@@ -48,7 +49,6 @@ export default props => {
 
     const getStockListCard = () => {
         let components = [];
-        let colorPrimary = useTheme().colors.primary
 
         aquisitions.forEach((obj, index) => {
             components.push(
@@ -61,25 +61,68 @@ export default props => {
                     colorDivider={colorPrimary}
                     colorTitle={colorPrimary}
                     onToggleSwitch={onToggleSwitch} />
-                )
-            }
+            )
+        }
         )
 
         return components;
     }
 
+    const renderItemAquisition = ({ item, index }) => {
+        let colorPrimary = '#ffd342'
+        return (
+            <CardStockComponent
+                key={index}
+                indexKey={index}
+                obj={item}
+                lengthList={aquisitions.length}
+                colorSwitch={colorPrimary}
+                colorDivider={colorPrimary}
+                colorTitle={colorPrimary}
+                onToggleSwitch={onToggleSwitch} />
+        )
+    }
+
+    const getAquisitionsByCategory = (idCategory) => {
+        let listAquisitionsNew = []
+
+        aquisitions.forEach(obj => {
+            if (obj.stock.category.idCategory === idCategory)
+                listAquisitionsNew.push(obj);
+        })
+
+        return listAquisitionsNew;
+    }
+
+    const renderFlatListByCategory = (idCategory) => {
+        return (
+            <FlatList
+                horizontal={true}
+                data={getAquisitionsByCategory(idCategory)}
+                renderItem={renderItemAquisition}
+                keyExtractor={item => item.idAquisition.toString()}
+            />
+        )
+    }
+
     return (
         <View style={{ backgroundColor: useTheme().colors.viewBackground, flex: 1 }}>
             <ScrollView>
-                <Appbar.Header>
-                    <Appbar.Content title={description} subtitle={"Ativos"} style={{ alignItems: 'center' }} />
-                </Appbar.Header>
-                <Paragraph>Adicione seus ativos aqui!</Paragraph>
-                <Paragraph>Qualquer ação é salva automaticamente</Paragraph>
+                <View>
+                    <Appbar.Header>
+                        <Appbar.Content title={description} subtitle={"Ativos"} style={{ alignItems: 'center' }} />
+                    </Appbar.Header>
+                    <Paragraph>Adicione seus ativos aqui!</Paragraph>
+                    <Paragraph>Qualquer ação é salva automaticamente</Paragraph>
 
-                <View >
-                    <View style={{ flex: 1 }}>
-                        {getStockListCard()}
+
+                    <View >
+                        {renderFlatListByCategory(1)}
+                        {renderFlatListByCategory(2)}
+                        {renderFlatListByCategory(3)}
+                        {renderFlatListByCategory(4)}
+                        {renderFlatListByCategory(5)}
+                        {renderFlatListByCategory(6)}
                     </View>
                 </View>
             </ScrollView>
@@ -116,3 +159,5 @@ export default props => {
         </View>
     )
 }
+
+//{getStockListCard()}
